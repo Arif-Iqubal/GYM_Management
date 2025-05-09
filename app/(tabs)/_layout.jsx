dc9e3f32a38a9136b0633d8059100426ab94b1b6
Arif-Iquba'l <arif04102001@gmail.com>
2025-05-09 23:07:15 +0530

    Testing branch Arifdev

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -8,11 +8,11 @@ import Foundation from '@expo/vector-icons/Foundation';
 export default function Layout() {
   return (
    <Tabs screenOptions={{headerShown:false}}>
-    <Tabs.Screen name='Home' options={{tabBarIcon : ()=> <Entypo name="home" size={24} color="black" />}}/>
-    <Tabs.Screen name='Member' options={{tabBarIcon : ()=> <MaterialIcons name="people" size={24} color="black" />}}/>
-    <Tabs.Screen name='Addmember' options={{tabBarIcon : ()=> <Feather name="plus" size={24} color="black" />}}/>
-    <Tabs.Screen name='Dashboard' options={{tabBarIcon : ()=> <Foundation name="graph-pie" size={24} color="black" />}}/>
-    <Tabs.Screen name='Profile' options={{tabBarIcon : ()=> <Feather name="user" size={24} color="black" />}}/>
+    <Tabs.Screen name='home' options={{tabBarIcon : ()=> <Entypo name="home" size={24} color="black" />}}/>
+    <Tabs.Screen name='member' options={{tabBarIcon : ()=> <MaterialIcons name="people" size={24} color="black" />}}/>
+    <Tabs.Screen name='addmember' options={{tabBarIcon : ()=> <Feather name="plus" size={24} color="black" />}}/>
+    <Tabs.Screen name='dashboard' options={{tabBarIcon : ()=> <Foundation name="graph-pie" size={24} color="black" />}}/>
+    <Tabs.Screen name='profile' options={{tabBarIcon : ()=> <Feather name="user" size={24} color="black" />}}/>
    </Tabs>
   );
 }
